0d617092b1d8a2d55064bde5ca7242dbda759d14
espg <refuge@rocktalus.com>
2025-02-21 11:46:23 -0500

    path fix escape

--- mortie/tests/test_morton_counts.py
+++ mortie/tests/test_morton_counts.py
@@ -3,13 +3,8 @@ import numpy as np
 import pandas as pd
 import healpy as hp
 
-basins = pd.read_csv('tests/Ant_Grounded_DrainageSystem_Polygons.txt',
-                     names=['Lat','Lon','basin'], delim_whitespace=True)
-b4 = basins[basins.basin == 4]
-
 def test_healpy_install():
     nside = 2**6
-    b2idx = hp.ang2pix(nside,b4.Lon.values, b4.Lat.values, lonlat=True) 
-    subset = np.unique(b2idx).ravel()
-    assert len(subset) == 39
+    b2idx = hp.ang2pix(nside,"132.85", "77.32", lonlat=True) 
+    assert len(subset) == 1
 
